c70b936302e3c98c4497586ba7df64b20d56281e
Corbyyy <anboakye004@st.ug.edu.gh>
2021-11-17 19:17:06 +0800

    Initial commit

---
+++ src/consts/cars.js
@@ -0,0 +1,49 @@
+const cars = [
+    {
+      id: '1',
+      name: 'Posche',
+      image: require('../assets/img1.jpg'),
+      price: '$500000',
+      liked: true,
+    },
+    {
+      id: '2',
+      name: 'Ferrari',
+      image: require('../assets/img2.jpg'),
+      price: '$400000',
+      liked: false,
+    },
+    {
+      id: '3',
+      name: 'Benz',
+      image: require('../assets/img3.jpg'),
+      price: '$200000',
+      liked: true,
+    },
+    {
+      id: '4',
+      name: 'Bugatti',
+      image: require('../assets/img5.jpg'),
+      price: '$150000',
+      liked: true,
+    },
+
+    {
+      id: '3',
+      name: 'Audi',
+      image: require('../assets/img6.jpg'),
+      price: '$100000',
+      liked: true,
+    },
+    {
+      id: '4',
+      name: 'Audi',
+      image: require('../assets/img4.jpg'),
+      price: '$100000',
+      liked: true,
+    },
+   
+  ];
+  
+  export default cars;
+  
